50f34fe8c675b5a4bc583131bdbf9329b8838dad
Stuart Pineo <stuartpineo@stuarts-imac.fios-router.home>
2016-04-27 07:56:33 -0400

    Completed core MixAssoc delete functionality. Refs #B49.

--- AssocTableViewController.m
+++ AssocTableViewController.m
@@ -584,7 +584,6 @@ const int ASSOC_COLORS_TAG     = 4;
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         
         MixAssocSwatch *mixAssocSwatch = [_mixAssocSwatches objectAtIndex:indexPath.row];
-        
         PaintSwatches *paintSwatch = (PaintSwatches *)[mixAssocSwatch paint_swatch];
         
         // Delete only if it references just this mix association
@@ -593,14 +592,16 @@ const int ASSOC_COLORS_TAG     = 4;
             [self.context deleteObject:paintSwatch];
             
         } else {
-            [paintSwatch delete:mixAssocSwatch];
+            [paintSwatch removeMix_assoc_swatchObject:mixAssocSwatch];
         }
         
+        [_mixAssociation removeMix_assoc_swatchObject:mixAssocSwatch];
         [self.context deleteObject:mixAssocSwatch];
         
         if ([_mixAssocSwatches count] == 1) {;
             [self.context deleteObject:_mixAssociation];
         }
+    
         _mixAssocSwatches = (NSMutableArray *)[[[_mixAssociation mix_assoc_swatch] allObjects] sortedArrayUsingDescriptors:@[_orderSort]];
         
         //[_mixAssocSwatches removeObjectAtIndex:indexPath.row];
@@ -609,43 +610,8 @@ const int ASSOC_COLORS_TAG     = 4;
             int mix_order = i + 1;
             [[_mixAssocSwatches objectAtIndex:i] setMix_order:[NSNumber numberWithInt:mix_order]];
         }
-
-//        PaintSwatches *swatchObj = [_paintSwatches objectAtIndex:indexPath.row];
-//        BOOL is_selected = [[swatchObj is_selected] boolValue];
-//        
-//        // Remove the swatch and cascade delete the mixassocswatch
-//        //
-//        if (is_selected == FALSE) {
-//            [self.context deleteObject:swatchObj];
-//        
-//        // Remove the mixassocswatch only (as swatch is associated with another mix)
-//        //
-//        } else {
-//            NSSet *assocSwatchSet = swatchObj.mix_assoc_swatch;
-//            for (MixAssocSwatch *obj in assocSwatchSet) {
-//                PaintSwatches *ps = (PaintSwatches *)obj.paint_swatch;
-//                MixAssociation *ma = obj.mix_association;
-//                if (([ps.name isEqualToString:swatchObj.name]) && (_mixAssociation.objectID == ma.objectID)) {
-//                    
-//                    // Need to delete set element for ps.paint_swatch
-//                    // Need to delete reference for ma.mix_association
-//                    
-//                    [self.context deleteObject:obj];
-//                }
-//            }
-//        }
-//        [_paintSwatches removeObjectAtIndex:indexPath.row];
-//            
-//        if ([_paintSwatches count] > 0) {
-//            [self recalculateOrder];
-//            
-//        } else {
-//            [self.context deleteObject:_mixAssociation];
-//            _mixAssociation = nil;
-//        }
         
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-//        [self.tableView reloadData];
         
       
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
@@ -894,10 +860,12 @@ const int ASSOC_COLORS_TAG     = 4;
         _mixAssocDesc = textField.text;
         
     } else {
-        for (int i=0; i<[_paintSwatches count]; i++) {
+        for (int i=0; i<[_mixAssocSwatches count]; i++) {
             int COLOR_TAG = i + ASSOC_COLORS_TAG;
             if (textField.tag == COLOR_TAG) {
-                [[_paintSwatches objectAtIndex:i] setName:textField.text];
+                MixAssocSwatch *mixAssocSwatch = [_mixAssocSwatches objectAtIndex:i];
+                PaintSwatches *paintSwatch = (PaintSwatches *)[mixAssocSwatch paint_swatch];
+                [paintSwatch setName:textField.text];
             }
         }
     }
