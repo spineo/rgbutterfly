217ca55b03d361e62d35e88a1939e097451a105a
Stuart Pineo <svpineo@gmail.com>
2017-02-04 08:54:07 -0500

    Commented out methods in NSData+Base64 that resulted in warning and not being used.

--- Custom/AssocDescTableViewCell.m
+++ Custom/AssocDescTableViewCell.m
@@ -21,6 +21,7 @@
 - (void)awakeFromNib {
     // Initialization code
     //
+    [super awakeFromNib];
 }
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
