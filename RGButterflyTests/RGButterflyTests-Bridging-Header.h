f300c2098bd235d5779c99632a45fc161f86a6eb
Stuart Pineo <svpineo@gmail.com>
2017-02-16 09:15:53 -0500

    Continued work on Unit tests (main ViewController). Refs #E121.

--- RGButterflyTests/RGButterflyTests-Bridging-Header.h
+++ RGButterflyTests/RGButterflyTests-Bridging-Header.h
@@ -2,4 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import <UIKit/UIKit.h>
+#import "GlobalSettings.h"
 #import "InitViewController.h"
+#import "ViewController.h"
