f4b18655d8e9a0861a92c9bf5f4cd681943999af
Stuart Pineo <svpineo@gmail.com>
2017-03-16 17:47:29 -0400

    General cleanup and unit tests to verify IBOutlets.

--- Controllers/ImageViewController.h
+++ Controllers/ImageViewController.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 Stuart Pineo. All rights reserved.
 //
 
-
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
@@ -31,8 +30,5 @@
 @property (nonatomic, strong) MatchAssociations *matchAssociation;
 
 @property (nonatomic, strong) IBOutlet UITableView *imageTableView;
-@property (nonatomic, weak) IBOutlet NSLayoutConstraint *scrollHeightConstraint;
-@property (nonatomic, weak) IBOutlet NSLayoutConstraint *tableHeightConstraint;
-@property (nonatomic, weak) IBOutlet NSLayoutConstraint *imageViewToSuperviewTop;
 
 @end
