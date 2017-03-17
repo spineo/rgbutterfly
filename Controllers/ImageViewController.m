09ff62ff69c28a5be90ddf98cddcfce1e03790da
Stuart Pineo <svpineo@gmail.com>
2017-03-17 15:32:30 -0400

    Fixed bug resulting in the match association name not property initialized in the Image VC view did load.

--- Controllers/ImageViewController.m
+++ Controllers/ImageViewController.m
@@ -294,11 +294,11 @@ CGFloat TABLEVIEW_BOTTOM_OFFSET = 100.0;
     _dragAreaEnabled = FALSE;
 
     
-    
     // Hide the "arrow" buttons by default
     //
     if (_matchAssociation != nil) {
         [self matchButtonsHide];
+        _assocName = [_matchAssociation name];
     }
 
     // Clear taps Alert Controller
@@ -2114,7 +2114,6 @@ CGFloat TABLEVIEW_BOTTOM_OFFSET = 100.0;
 
     // Applies to both updates and new
     //
-    //_assocName = [_matchAssociation name];
     if ([_assocName isEqualToString:@""] || _assocName == nil) {
         _assocName = [[NSString alloc] initWithFormat:@"MatchAssoc %@", [GenericUtils getCurrDateString:@"YYYY-MM-dd HH:mm:ss"]];
         ((UITextField *)[_updateAlertController.textFields objectAtIndex:0]).text = _assocName;
