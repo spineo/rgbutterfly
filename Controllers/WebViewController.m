7a4f46aee37e411feb62fa7aa419c53bc1debbc4
Stuart Pineo <svpineo@gmail.com>
2017-03-28 09:08:27 -0400

    Started implementation of the WebView (refs #E284)

---
+++ Controllers/WebViewController.m
@@ -0,0 +1,49 @@
+//
+//  WebViewController.m
+//  RGButterfly
+//
+//  Created by Stuart Pineo on 3/28/17.
+//  Copyright © 2017 Stuart Pineo. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [_webView setDelegate:self];
+    
+    NSString *urlString = @"http://www.google.com/";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:urlRequest];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goBack:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
