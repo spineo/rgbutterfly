0f51877df8b4c26b7b2bd7a0a60bee9e04224c18
Stuart Pineo <svpineo@gmail.com>
2017-06-24 20:44:38 -0400

    Created new custom Navigation controllers to disable landscape orientation in the About, Disclaimer, Settings, and Color View controllers.

---
+++ SettingsNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsNavigationController.h
+//  RGButterfly
+//
+//  Created by Stuart Pineo on 6/24/17.
+//  Copyright © 2017 Stuart Pineo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsNavigationController : UINavigationController
+
+@end
---
+++ SwatchDetailNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SwatchDetailNavigationController.h
+//  RGButterfly
+//
+//  Created by Stuart Pineo on 6/24/17.
+//  Copyright © 2017 Stuart Pineo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwatchDetailNavigationController : UINavigationController
+
+@end
